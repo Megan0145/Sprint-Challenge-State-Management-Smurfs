7075a51698e50e9486488da309b68f31010c0584
Megan <mennis0145@gmail.com>
2019-10-11 08:58:17 +0100

    getSmurfs action creator working as expected, returning smurfs from api, setting them to state in reducer, App component mapping over array and rendering smurf name

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { connect } from 'react-redux';
 import * as actionCreators from '../state/actionCreators';
 
-export function App() {
+export function App({ smurfs, getSmurfs }) {
+  useEffect(() => {
+    getSmurfs();
+  }, []);
   return (
     <div className="App">
       <h1>SMURFS! 2.0 W/ Redux</h1>
-      <div>Welcome to your state management version of Smurfs!</div>
-      <div>Start inside of your `src/index.js` file!</div>
-      <div>Have fun!</div>
+      <div>
+        {
+          smurfs.smurfs.map(smurf => {
+            return <p>{smurf.name}</p>
+          })
+        }
+      </div>
     </div>
   );
 }
--- smurfs/src/state/reducers.js
+++ smurfs/src/state/reducers.js
@@ -6,6 +6,8 @@ const initState = {
 
 export function smurfReducer(state = initState, action){
     switch (action.type){
+        case types.GET_SMURFS:
+            return {...state, smurfs: action.payload }
         default: 
         return state;
     }
